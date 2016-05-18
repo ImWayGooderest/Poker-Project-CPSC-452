b348506e7a806f3e8acb9af4a160f678c8309599
lightball20 <lightball20@yahoo.com>
2016-05-17 19:36:40 -0700

    Add files via upload
    Added new card1/card2/card3 for the player function, added a way to save the cards the user submits, and built a checkwinner function. All off it commented out and again not exactly using encryption in the slighest

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ function Player()
   this.session_key= 0;//the key actually used for encryption
   this.session_key_base64=0;
   this.hand = [];
+  //this.card1 = ""; //It looks like the cards are saved as strings
+  //this.card2 = "";
+  //this.card3 = "";
   this.score = 0;
 }
 
@@ -44,11 +47,77 @@ app.post("/sendCard", function (req,res) {
   var player = verify_user(req);
   if(player) {
     //removed req.card from player.hand and say ok or maybe send back the new hand
+	/*if (player.card1 == ""){player.card1 = reg.card;}
+	else if (player.card2 == ""){player.card2 = reg.card;}
+	else if (player.card3 == ""){player.card3 = reg.card;}
+	else {res.send("ERROR: More than 3 rounds")}
+	*/
   } else {
     res.send("ERROR: Invalid player")
   }
+  //player.hand = [];
   res.sendStatus(200);
 });
+
+/*
+app.post("/findWinner", function (req,res) {
+	console.log("POST /sendCard");
+	var player = verify_user(req);
+	if(player) {
+		player1Counter = 0;
+		player2Counter = 0;
+		draw = 0;
+		
+		//Check Card 1
+		if (player1.card1 > player2.card1){
+			player1Counter += 1;
+		}
+		else if (player1.card1 < player2.card1){
+			player2Counter += 1;
+		}
+		else if (player1.card1 == player2.card1){
+			draw += 1;
+		}
+		
+		//Check Card 2
+		if (player1.card2 > player2.card2){
+			player1Counter += 1;
+		}
+		else if (player1.card2 < player2.card2){
+			player2Counter += 1;
+		}
+		else if (player1.card2 == player2.card2){
+			draw += 1;
+		}
+		
+		//Check Card 3
+		if (player1.card3 > player2.card3){
+			player1Counter += 1;
+		}
+		else if (player1.card3 < player2.card3){
+			player2Counter += 1;
+		}
+		else if (player1.card3 == player2.card3){
+			draw += 1;
+		}
+		
+		
+		//Find Winner
+		if (player1Counter > player2Counter) {
+			winner = Player_1;
+		}
+		else if (player1Counter < player2Counter) {
+			winner = Player_2;
+		}
+		else if (player1Counter == player2Counter) {
+			winner = Draw;
+		}
+	} else {
+		res.send("ERROR: Invalid player")
+    }
+	 res.status(200).json(encrypt(winner));
+})
+*/
 
 //NOT TESTED
 function verify_user(req) {
