e554c1cc38b1eb90a9d739e006b9cd3ec50c7b75
lightball20 <lightball20@yahoo.com>
2016-05-17 19:34:57 -0700

    Add files via upload
    Added a while loop to submit cards and get a new hand then request  a winner. All of it is commented out and not exactly using the encrypt/decrypt correct.

--- client/client.py
+++ client/client.py
@@ -47,13 +47,20 @@ def main():
 	private_key, public_key = cipher.getKey()
 	myHand = base64.b64decode(bytes(loginAndGetHand(), encoding="ascii"))
 	# test stuff
-	myHand = cipher.decrypt(myHand, private_key).decode().split() #decrypts, then bytestring to string, then convert string to list
-	# myHand = getHand()	#assuming the randomized 3 cards return as a list
-	userInput = input("This is currently your hand, please select one: " + str(myHand))
-	# #print(myHand)	#somewhere here have user input?
-	# playCard(userInput)		#have the user input be passed on to the server
+	#while counter !=3:
+		myHand = cipher.decrypt(myHand, private_key).decode().split() #decrypts, then bytestring to string, then convert string to list
+		# myHand = getHand()	#assuming the randomized 3 cards return as a list
+		userInput = input("This is currently your hand, please select one: " + str(myHand))
+		# playCard(userInput)		#have the user input be passed on to the server
+		#counter += 1
+		#userInput = input("Get another hand?" Yes or No?)
+		#if (userInput == "Yes"):
+			#myHand = base64.b64decode(bytes(getHand(), encoding="ascii")) #Need new function simply for getting the hand and encoding it separate from logging in the first time
+	#userInput = input("Would you like to find who won?")
+	#if (userInput == "Yes"):
+		#winner = request.post("http://localhost:3000/findWinner", json=prepareData())
+		#print("The winner was:" + winner)
 
-	# hand = getHand()
 
 
 
